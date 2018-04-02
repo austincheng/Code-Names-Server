1dc7c0df218c0bb3029fd9224c2529d6aff9bc5e
Austin Cheng <austincheng0@gmail.com>
2018-04-02 02:55:42 -0700

    Initial commit with board UI

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var serv = require('http').Server(app);
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/client/index.html');
+});
+app.use('/client', express.static(__dirname + '/client'));
+
+serv.listen(process.env.PORT || 3000);
+console.log('Server started.');
+
+var io = require('socket.io')(serv, {});
+io.sockets.on('connection', function(socket) {
+
+});
